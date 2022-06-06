1c817c6c488b65f9535cb4fc1ee5ec795492d77c
Sayali <sayalijoshi30@gmail.com>
2022-06-06 20:56:46 +0530

    6th June

---
+++ Rearrange Products Table/Rearrange Products Table.sql
@@ -0,0 +1,13 @@
+/* Write your PL/SQL query statement below */
+
+select *
+from Products
+unpivot (
+    price
+    for store
+    in (
+        store1 as 'store1',
+        store2 as 'store2',
+        store3 as 'store3'
+    )
+);
